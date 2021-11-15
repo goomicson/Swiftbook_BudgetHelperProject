b73cfc130f4704d32f4b964f1c7076b0e836567d
Shevshelev Lev <shevshelev.lev@gmail.com>
2021-11-15 19:52:33 +0500

    user fixed

--- Swiftbook_BudgetHelperProject/Models/Model.swift
+++ Swiftbook_BudgetHelperProject/Models/Model.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct User {
+    let name: String
+    let account: Double
+}
+
+extension User {
+    static func getData() -> User {
+        let user = User(name: "Richard Gere", account: 125000000)
+        return user
+    }
+}
